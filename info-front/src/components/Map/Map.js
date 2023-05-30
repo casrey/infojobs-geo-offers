abeb43f5789cab849b53b5135e75d5c8460fcab5
Javier Reyes <javier.reyes@mercadolibre.com.co>
2023-05-29 20:13:29 -0500

    ✨ drawing cluster map

--- info-front/src/components/Map/Map.js
+++ info-front/src/components/Map/Map.js
@@ -2,31 +2,80 @@
 import { Card, Title } from "@tremor/react";
 import React, { useRef, useEffect, useState } from "react";
 import "./style.css";
+import {clusterLayer, clusterCountLayer, unclusteredPointLayer} from './layers';
+
+import { Map as MapReact, Source, Layer } from 'react-map-gl';
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 const Map = () => {
+  let mapRef;
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [lng, lat],
-      zoom: zoom,
+  // useEffect(() => {
+  //   if (map.current) return; // initialize map only once
+  //   map.current = new mapboxgl.Map({
+  //     container: mapContainer.current,
+  //     style: "mapbox://styles/mapbox/streets-v12",
+  //     center: [lng, lat],
+  //     zoom: zoom,
+  //   });
+  // }, []);
+
+  const onClick = event => {
+    const feature = event.features[0];
+    const clusterId = feature.properties.cluster_id;
+
+    const mapboxSource = mapRef.current.getSource('earthquakes');
+
+    mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
+      if (err) {
+        return;
+      }
+
+      mapRef.current.easeTo({
+        center: feature.geometry.coordinates,
+        zoom,
+        duration: 500
+      });
     });
-  }, []);
+  };
 
   return (
     <Card className="h-full">
       <Title>Jobs For You</Title>
-      <div ref={mapContainer} className="map-container" />
+      {/* <div ref={mapContainer} className="map-container" /> */}
+
+      <MapReact
+        initialViewState={{
+          latitude: 40.67,
+          longitude: -103.59,
+          zoom: 3
+        }}
+        mapStyle="mapbox://styles/mapbox/streets-v12"
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        interactiveLayerIds={[clusterLayer.id]}
+        onClick={onClick}
+        ref={mapRef}
+      >
+        <Source
+          id="earthquakes"
+          type="geojson"
+          data="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
+          cluster={true}
+          clusterMaxZoom={14}
+          clusterRadius={50}
+        >
+          <Layer {...clusterLayer} />
+          <Layer {...clusterCountLayer} />
+          <Layer {...unclusteredPointLayer} />
+        </Source>
+      </MapReact>
     </Card>
   );
 };
