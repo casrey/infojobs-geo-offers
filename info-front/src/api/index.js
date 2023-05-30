3c95ffe59307afd9ce3ea6418fa33ee6fb198a17
Diego Castillo <diegorlando5@gmail.com>
2023-05-30 16:45:25 -0500

    vercel cloud

--- info-front/src/api/index.js
+++ info-front/src/api/index.js
@@ -2,12 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 
 import { serializeAdvanceFilters } from "../utils";
 
+const rootUrl =
+  process.env.NODE_ENV === "production" ? "http://api.infojobs.net/api/9/" : "";
+
 export const OFFERS_QUERY = ({ searchParam, advancedFilters }) => {
   return useQuery({
     queryKey: ["offers"],
     queryFn: async () => {
       const data = await fetch(
-        `/offer?q=${searchParam}${
+        `${rootUrl}/offer?q=${searchParam}${
           serializeAdvanceFilters(advancedFilters)
             ? `&${serializeAdvanceFilters(advancedFilters)}`
             : ""
